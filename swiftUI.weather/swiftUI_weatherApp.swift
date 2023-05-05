4925584d01d92da3dfdf423ed869e7d29c754505
Salm Do <thaodo.usa@gmail.com>
2023-05-05 06:19:00 -0500

    init code

---
+++ swiftUI.weather/swiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_weatherApp.swift
+//  swiftUI.weather
+//
+//  Created by Salmdo on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
